6e816dae678da5ea3560558ce4dbe306923eb579
Mohit Goyal <mohit.g.mg1996@gmail.com>
2022-07-03 11:22:22 +0530

    probability calculation

--- prisoners.js
+++ prisoners.js
@@ -6,7 +6,7 @@ const rl = readline.createInterface({
 });
 console.log("Enter to number of prisoners you want to get freed?");
 rl.question(
-  "NOTE: It is suggested to keep number of prisoners to be more then 50 and in even number:\n",
+  "NOTE: Since It's Node Application\nNOTE: It is suggested to keep number of prisoners to be less then equal to 30000 and in even number:\n",
   (number_of_prisoners) => {
     try {
       if (isNaN(number_of_prisoners)) {
@@ -31,6 +31,7 @@ class NPrisonerProblem {
     this.createDrawer();
     this.enterPrisonerSequentially();
     this.checkIsPrisonerCanFreed();
+    this.calculateProbability();
   }
 
   createDrawer() {
@@ -79,7 +80,6 @@ class NPrisonerProblem {
     console.log("-----------------------------------------------");
     console.log("prisoner number:", number);
     console.log("Checked Drawers:", drawers.length);
-    console.log("check:", drawers);
     console.log("-----------------------------------------------");
   }
 
@@ -88,5 +88,28 @@ class NPrisonerProblem {
       "\n\n\nIs Prisoner Freed",
       this.isFreed.every((d) => d)
     );
+  }
+
+  calculateProbability() {
+    const probability_of_failure = this.submissionOfN(
+      parseInt(this.prisoners / 2) + 1,
+      this.prisoners
+    );
+
+    console.log("Probability of success(As wiki):", 1 - probability_of_failure);
+    console.log(
+      "Probability of success for current cycle:",
+      this.isFreed.filter((d) => d).length / this.prisoners
+    );
+  }
+
+  submissionOfN(from, to) {
+    let n = from;
+    let sum = 0;
+    while (n <= to) {
+      sum += 1 / n;
+      n++;
+    }
+    return sum;
   }
 }
